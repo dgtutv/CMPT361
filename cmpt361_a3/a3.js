2f3ff7f805cc1b765368e56c4a1e1d8649044c66
Daniel <dgt1@sfu.ca>
2022-11-10 14:03:56 -0800

    started work on re-doing rasterization, making use of classes

--- cmpt361_a3/a3.js
+++ cmpt361_a3/a3.js
@@ -5,20 +5,24 @@ import { Rasterizer } from './rasterizer.js';
 ////////////////////////////////////////////////////////////////////////////////
 // TODO: Implement functions drawLine(v1, v2) and drawTriangle(v1, v2, v3) below.
 ////////////////////////////////////////////////////////////////////////////////
-
+//Class for vertices
+class vertex{
+	constructor(x, y, [r, g, b]){
+	this.x = x;
+	this.y = y;
+	this.c = [r,g,b];
+	}
+}
 //Function to determine color of line at pixel v
-function linePixelColor(v1, v2, x, y){
-    //defining variables from input
-    const [x1, y1, [r1, g1, b1]] = v1;
-    const [x2, y2, [r2, g2, b2]] = v2;
+function getColor(a, b, x, y){
     //Find normalized distance along line pixel v is
-    var totalDistance = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-    var distanceVtoV2 = Math.sqrt((x-x1)^2 + (y-y1)^2);
+    var totalDistance = Math.sqrt((b.x-a.x)^2 + (b.y-a.y)^2);
+    var distanceVtoV2 = Math.sqrt((x-a.x)^2 + (y-a.y)^2);
     var normDist = distanceVtoV2/totalDistance;
     //Apply linear interpolation for distance of pixel v along the line.
-    var R = r1 + (r2-r1)*normDist;
-    var G = g1 + (g2-g1)*normDist;
-    var B = b1 + (b2-b1)*normDist;
+    var R = a.c[0] + (b.c[0]-a.c[0])*normDist;
+    var G = a.c[1] + (b.c[1]-a.c[1])*normDist;
+    var B = a.c[2] + (b.c[2]-a.c[2])*normDist;
     return [R, G, B];
 }
 
@@ -29,6 +33,23 @@ Rasterizer.prototype.drawLine = function(v1, v2) {
     // TODO/HINT: use this.setPixel(x, y, color) in this function to draw line
     this.setPixel(Math.floor(x1), Math.floor(y1), [r1, g1, b1]);
     this.setPixel(Math.floor(x2), Math.floor(y2), [r2, g2, b2]);
+    //Determine the starting point(a) and end point(b)
+    var a, b;
+    if(x1>x2){
+        a = new vertex(x2, y2, [r2, g2, b2]);
+        b = new vertex(x1, y1, [r1, g1, b1]);
+    }
+    else{
+		a = new vertex(x1, y1, [r1, g1, b1]);
+		b = new vertex(x2, y2, [r2, g2, b2]);
+	}
+	//When m is undefined(x1 == x2), then draw a vertical line
+	if(a.x==b.x){
+		var x = a.x;
+		for(var y = a.y; y<b.y; ++y){
+			this.setPixel(Math.floor(x), Math.floor(y), getColor(a, b, x, y));
+		}
+	}
 }
 
 // take 3 vertices defining a solid triangle and rasterize to framebuffer
